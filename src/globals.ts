0c223a543140506bf396708c30719cb086b61d01
mrquackduck <mrquackduckk@gmail.com>
2023-07-15 12:03:43 +0300

    Update

--- src/globals.ts
+++ src/globals.ts
@@ -2,6 +2,6 @@
 
 export const API_URL = 'http://localhost:5228';
 export const LANGUAGES = [
-    {key: '🇬🇧 English', value: 0},
+    {key: '🇺🇸 English', value: 0},
     {key: '🇺🇦 Ukrainian', value: 1},
 ];
